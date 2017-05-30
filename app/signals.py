fe5f8f035a9e53ce4b35087f6c4b76c2161692aa
Sergio Leiva <sergio.leiva@ing.uchile.cl>
2017-05-30 00:10:58 -0400

    Remove print method

--- app/signals.py
+++ app/signals.py
@@ -10,23 +10,3 @@ dict_favoritos = {}
 @receiver(pre_save, sender=Comprador)
 def preActualizarFavoritos(sender, instance, **kwargs):
     dict_favoritos[instance.unique_id] = instance.favoritos.all()
-
-
-@receiver(post_save, sender=Comprador)
-def postActualizarFavoritos(sender, instance, **kwargs):
-    print("Largo post es:")
-    print(len(instance.favoritos.all()))
-
-    #if len(instance.favoritos.all()) == len(dict_favoritos[instance.unique_id]):
-    #    return
-
-    #elif len(instance.favoritos.all()) > len(dict_favoritos[instance.unique_id]):
-    #    for i in instance.favoritos.all():
-    #        if i not in dict_favoritos[instance.unique_id]:
-    #            i.favoritos += 1
-
-    #elif len(instance.favoritos.all()) < len(dict_favoritos[instance.unique_id]):
-    #    for i in dict_favoritos[instance.unique_id]:
-    #        if i not in instance.favoritos.all():
-    #            i.favoritos -= 1
-
